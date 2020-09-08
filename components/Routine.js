86bce24c5c17c0dfd7f7959c05f12d6504966a3a
cesar r <cesaracer@gmail.com>
2020-09-07 21:21:43 -0600

    added button

--- components/Routine.js
+++ components/Routine.js
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Workout from './Workout'
-import { View, Text } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import { View, Text, ScrollView, StyleSheet, AsyncStorage } from 'react-native'
 import { connect } from 'react-redux'
+import { TouchableWithoutFeedback, TouchableOpacity } from 'react-native-gesture-handler'
 
 function Routine({routine}){
+    
+    const handleClick = () => {
+        let res = AsyncStorage.getItem('bruh')
+        alert(res)
+    }
+
     return(
-        <View>
-            <Text>{routine.name}</Text>
+        <View style={styles.page}>
             <ScrollView>
                 {
                     routine.workouts.map(w => <Workout name={w.name} duration={w.duration}/>)
                 }
             </ScrollView>
+            <TouchableOpacity style={styles.btn_edit}>
+                <Text style={styles.text}>Edit</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    btn_edit: {
+        backgroundColor: 'orange',
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: 15,
+        right: -30,
+        alignSelf: 'flex-end'
+        
+    },
+    page: {
+        width: '80%',
+        height: '100%',
+        marginLeft: '10%',
+        flexDirection: 'column'
+    },
+    text: {
+        color: 'white',
+        fontSize: 20
+    }
+})
 
 const mapStateToProps = (state) => {
     return{
