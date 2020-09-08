3e5397dfaa26fd7ede8c69af07b12e5290870890
cesar r <cesaracer@gmail.com>
2020-09-07 19:30:00 -0600

    added action type

--- actions/actionTypes.js
+++ actions/actionTypes.js
@@ -1,4 +1,7 @@
+import { exp } from "react-native-reanimated"
+
 export const ADD_ROUTINE = 'ADD_ROUTINE'
 export const DEL_ROUTINE = 'DEL_ROUTINE'
 export const EDIT_ROUTINE = 'EDIT_ROUTINE'
 export const SET_ROUTINE = 'SET_ROUTINE'
+export const FETCH = 'FETCH'
