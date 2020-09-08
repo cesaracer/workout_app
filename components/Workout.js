c77973ada663ff92433e8c979eeb77328ffe31ff
cesar r <cesaracer@gmail.com>
2020-09-07 21:15:11 -0600

    added styling

--- components/Workout.js
+++ components/Workout.js
@@ -1,13 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 function Workout({name, duration}){
     return(
-        <View>
-            <Text>{name}</Text>
-            <Text>{duration}</Text>
+        <View style={styles.workout}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.time}>{duration} Minutes</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    workout: {
+        flex: 1, 
+        flexDirection: 'row',
+        width: '100%',
+        height: 50,
+        marginTop: 15,
+        marginBottom: 10,
+        justifyContent: 'space-between',
+        borderWidth: 2,
+        borderColor: 'orange',
+        borderRadius: 8
+    },
+    name: {
+        color: 'orange',
+        marginLeft: 10,
+        alignSelf: 'center'
+    },
+    time: {
+        color: 'orange',
+        marginRight: 10,
+        alignSelf: 'center'
+    }
+
+})
 
 export default Workout
