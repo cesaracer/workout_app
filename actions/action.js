23a54759ec73ba33c22b6569848569f01c6c2f76
cesar r <cesaracer@gmail.com>
2020-09-07 19:26:56 -0600

    added api call

--- actions/action.js
+++ actions/action.js
@@ -1,4 +1,5 @@
-import {ADD_ROUTINE, DEL_ROUTINE, EDIT_ROUTINE, SET_ROUTINE} from './actionTypes'
+import {ADD_ROUTINE, DEL_ROUTINE, EDIT_ROUTINE, SET_ROUTINE, FETCH} from './actionTypes'
+import Axios from 'axios'
 
 export const addRoutine = (routine) => {
     return{
@@ -26,5 +27,22 @@ export const setRoutine = (routine) => {
     return{
         type: SET_ROUTINE,
         routine
+    }
+}
+
+export const setRoutines = (routines) => {
+    return{
+        type: FETCH,
+        routines
+    }
+}
+
+export function loadRoutines(){
+    return(dispatch) => {
+        Axios.get('https://us-central1-routine-app-99182.cloudfunctions.net/app/routines/all')
+        .then(result => {
+            const res = result.data
+            dispatch(setRoutines(res))
+        })
     }
 }
